2314895497280473ba45ce955e50d4ac602daefe
kayuela <56114643+kayuela@users.noreply.github.com>
2020-01-25 12:14:42 +0100

    Añadido campo de distancia real en la screen de inputs

--- Application/build/generated/not_namespaced_r_class_sources/debug/r/androidx/loader/R.java
+++ Application/build/generated/not_namespaced_r_class_sources/debug/r/androidx/loader/R.java
@@ -97,15 +97,15 @@ public final class R {
         public static final int notification_background = 0x7f080088;
         public static final int notification_main_column = 0x7f080089;
         public static final int notification_main_column_container = 0x7f08008a;
-        public static final int right_icon = 0x7f0800aa;
-        public static final int right_side = 0x7f0800ab;
-        public static final int tag_transition_group = 0x7f0800e4;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800e5;
-        public static final int tag_unhandled_key_listeners = 0x7f0800e6;
-        public static final int text = 0x7f0800e7;
-        public static final int text2 = 0x7f0800e8;
-        public static final int time = 0x7f0800f0;
-        public static final int title = 0x7f0800f5;
+        public static final int right_icon = 0x7f0800ac;
+        public static final int right_side = 0x7f0800ad;
+        public static final int tag_transition_group = 0x7f0800e6;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800e7;
+        public static final int tag_unhandled_key_listeners = 0x7f0800e8;
+        public static final int text = 0x7f0800e9;
+        public static final int text2 = 0x7f0800ea;
+        public static final int time = 0x7f0800f2;
+        public static final int title = 0x7f0800f7;
     }
     public static final class integer {
         private integer() {}
@@ -125,7 +125,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0d0050;
+        public static final int status_bar_notification_info_overflow = 0x7f0d0051;
     }
     public static final class style {
         private style() {}
